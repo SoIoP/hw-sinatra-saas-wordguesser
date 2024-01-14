8adde740c30ab346bc99bd36c7768dd59a679446
SoloP <ttrrvv.pp@gmail.com>
2024-01-14 14:02:49 +0000

    test

--- app.rb
+++ app.rb
@@ -6,25 +6,25 @@ class WordGuesserApp < Sinatra::Base
 
   enable :sessions
   register Sinatra::Flash
-  
+
   before do
     @game = session[:game] || WordGuesserGame.new('')
   end
-  
+
   after do
     session[:game] = @game
   end
-  
+
   # These two routes are good examples of Sinatra syntax
   # to help you with the rest of the assignment
   get '/' do
     redirect '/new'
   end
-  
+
   get '/new' do
     erb :new
   end
-  
+
   post '/create' do
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || WordGuesserGame.get_random_word
@@ -33,16 +33,28 @@ class WordGuesserApp < Sinatra::Base
     @game = WordGuesserGame.new(word)
     redirect '/show'
   end
-  
+
   # Use existing methods in WordGuesserGame to process a guess.
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      result = @game.guess(letter)
+    rescue ArgumentError => e
+      flash[:message] = "Invalid guess: #{e.message}"
+      redirect '/show'
+    end
+
+    case result
+    when :repeated
+      flash[:message] = "You have already used that letter."
+    end
+
     redirect '/show'
   end
-  
+
   # Everytime a guess is made, we should eventually end up at this route.
   # Use existing methods in WordGuesserGame to check if player has
   # won, lost, or neither, and take the appropriate action.
@@ -50,17 +62,27 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    game_status = @game.check_game_status
+
+    case game_status
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show
+    end
   end
-  
+
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    erb :win
   end
-  
+
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    erb :lose
   end
-  
+
+  # Start game
+  run! if app_file == $0
+
 end
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,9 +4,52 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_reader :word, :guesses, :wrong_guesses
 
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    validate_letter(letter)
+
+    letter = letter.downcase
+    update_guesses(letter) if correct_guess?(letter)
+    update_wrong_guesses(letter) if incorrect_guess?(letter)
+  end
+
+  def word_with_guesses
+    @word.chars.map { |char| @guesses.include?(char) ? char : '_' }.join
+  end
+
+  def check_game_status
+    return :win if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
+  end
+
+  private
+
+  def validate_letter(letter)
+    raise ArgumentError if letter.nil? || letter !~ /\A[a-zA-Z]\Z/i
+  end
+
+  def correct_guess?(letter)
+    @word.include?(letter) && !@guesses.include?(letter)
+  end
+
+  def update_guesses(letter)
+    @guesses += letter
+  end
+
+  def incorrect_guess?(letter)
+    !@word.include?(letter) && !@wrong_guesses.include?(letter)
+  end
+
+  def update_wrong_guesses(letter)
+    @wrong_guesses += letter
   end
 
   # You can test it by installing irb via $ gem install irb
